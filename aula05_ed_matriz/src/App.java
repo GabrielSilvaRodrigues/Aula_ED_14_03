e3dd46cdd33e7413a42adb5d781b6bad5b7f2fdc
Santosdaniel57 <169057592+GabrielSilvaRodrigues@users.noreply.github.com>
2025-03-14 10:41:07 -0300

    [feat] Update App.java
    Inserindo anotações e percorrendo a matriz mostrando os dados inseridos.

--- aula05_ed_matriz/src/App.java
+++ aula05_ed_matriz/src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int[][] matriz = new int[3][2];
+        
+        //Acesso direto: linha e coluna
+        //Hard code ou código engessado
         matriz[0][0]=1;
         matriz[0][1]=2;
 
@@ -9,5 +12,21 @@ public class App {
 
         matriz[2][0]=5;
         matriz[2][1]=6;
+
+        //Tipagem incorreta
+        //matriz[2][1]="7";
+        
+        // Estourando a matriz: informar uma linha ou coluna inexistente
+        //matriz[2][2]=7;
+        
+        //Percorrendo uma matriz
+        //connsultar elementos
+        //Incerir elementos
+        for(int linha=0; linha<matriz.length;linha++){
+            for(int col=0; col<matriz[linha].length; col++){
+                System.out.print(matriz[linha][col]+" ");
+            }
+            System.out.println("");
+        }
     }
 }
