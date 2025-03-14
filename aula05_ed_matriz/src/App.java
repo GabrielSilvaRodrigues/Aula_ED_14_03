fc574cdb085b88d8c8922fbcc1eb801827c84c55
Santosdaniel57 <169057592+GabrielSilvaRodrigues@users.noreply.github.com>
2025-03-14 10:14:55 -0300

    [feat] Creat matriz
    criando matriz em Java.

---
+++ aula05_ed_matriz/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[][] matriz = new int[3][2];
+        matriz[0][0]=1;
+        matriz[0][1]=2;
+
+        matriz[1][0]=3;
+        matriz[1][1]=4;
+
+        matriz[2][0]=5;
+        matriz[2][1]=6;
+    }
+}
